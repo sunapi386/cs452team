a50d98de40297880c0454923389e849a8f5cdd61
Jason Sun <jason.sun@uwaterloo.cam>
2015-05-20 13:46:03 -0400

    cpsr.h contains thumb

--- cpsr.h
+++ cpsr.h
@@ -10,13 +10,15 @@
     #define UserMode 0x10
     #define FIQMode 0x11
     #define IRQMode 0x12
-    #define SupervisorMode 0x13
-    #define AbortMode 0x17
-    #define UndefinedMode 0x1b
-    #define SystemMode 0x1f
-    
+    #define SprMode 0x13
+    #define AbtMode 0x17
+    #define UdfMode 0x1b
+    #define SysMode 0x1f
+
 #define DisableIRQ (1<<7)
 #define DisableFIQ (1<<6)
 
+
+#define Thumb (1<<5)
 
 #endif /*_ CPSR_H */
