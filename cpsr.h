d049ccda119e278fa83549a69caa60f1e6061ebd
Jason Sun <jason.sun@uwaterloo.cam>
2015-05-19 20:06:56 -0400

    add cpsr.h

---
+++ cpsr.h
@@ -0,0 +1,22 @@
+fndef __CPSR_H
+#define __CPSR_H 1
+ 
+#define NegativeMask (1<<31)
+#define ZeroMask (1<<30)
+#define CarryMask (1<<29)
+#define OverflowMask (1<<28)
+
+#define ModeMask 0x1f
+    #define UserMode 0x10
+    #define FIQMode 0x11
+    #define IRQMode 0x12
+    #define SupervisorMode 0x13
+    #define AbortMode 0x17
+    #define UndefinedMode 0x1b
+    #define SystemMode 0x1f
+    
+#define DisableIRQ (1<<7)
+#define DisableFIQ (1<<6)
+
+
+#endif /*_ CPSR_H */
