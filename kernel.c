e8786527e3f95dc5adb8053674ca7c70f84bb365
Jason Sun <jason.sun@uwaterloo.cam>
2015-05-20 21:13:54 -0400

    inline asm c-switch in kernel.c

--- kernel.c
+++ kernel.c
@@ -5,57 +5,84 @@
 #include <task.h>
 #include <scheduler.h>
 
-void firstTask() {
-    // asm volatile( "swi 0" ); //  c-switch
+// void firstTask() {
+//     // asm volatile( "swi 0" ); //  c-switch
 
-    bwprintf( COM2, "UserTask\n" );
-}
+//     bwprintf( COM2, "UserTask\n" );
+// }
 
-void InitKernel(TaskDescriptor pool[])
-{
-    // Initialize swi jump table
-    //*(unsigned int)(0x28) = &kernel_entry;    
-    
-    // Initialize tasks
-    int i;
-    for (i = 0; i < MAX_NUM_TASKS; i++)
-    {
-        TaskDescriptor *task = &pool[i];
-        task->id = i;
-        task->parent_id = 0;
-        task->priority = 0;
-        task->ret = 0;
-        task->sp = NULL;
-        task->cpsr = 0;
-        task->next = NULL;
-    }
+// void InitKernel(TaskDescriptor pool[])
+// {
+//     // Initialize swi jump table
+//     //*(unsigned int)(0x28) = &kernel_entry;
 
-    InitScheduler();
-}
+//     // Initialize tasks
+//     int i;
+//     for (i = 0; i < MAX_NUM_TASKS; i++)
+//     {
+//         TaskDescriptor *task = &pool[i];
+//         task->id = i;
+//         task->parent_id = 0;
+//         task->priority = 0;
+//         task->ret = 0;
+//         task->sp = NULL;
+//         task->cpsr = 0;
+//         task->next = NULL;
+//     }
 
-void HandleRequest(int request)
-{
+//     InitScheduler();
+// }
 
-}
+// void HandleRequest(int request)
+// {
+
+// }
 
 int main()
 {
-    TaskDescriptor taskPool[MAX_NUM_TASKS];
-    InitKernel(taskPool);
-    
-    for (;;)
-    {
-        int request = 0;
-        TaskDescriptor *task = Scheduler();
-        
-        if (task == NULL)
-        {
-            break;
-        }
+     asm volatile(
+        "KernelExit:\n\t"
+        "mov ip, sp\n\t"
+        "stmfd sp!, {fp, ip, lr, pc}\n\t"  // save kregs on kstack
+        "sub fp, ip, #4\n\t"
+        "sub sp, sp, #12\n\t"
+        "str r1, [fp, #-24]\n\t"
+        "str r0, [fp, #-20]\n\t"
+        "str r1, [fp, #-24]\n\t"
+        "ldr r3, [fp, #-20]\n\t"
+        "ldr r3, [r3, #20]\n\t"
+        "str r3, [fp, #-16]\n\t"
+        "ldr r3, [fp, #-16]\n\t"
+        "mov r0, #1\n\t"
+        "mov r1, r3\n\t"
+        "bl bwputr(PLT)\n\t"
+        "sub sp, fp, #12\n\t"
+        "ldmfd sp, {fp, sp, pc}\n\t"
+        "KernelEnter:\n\t"
+        "mov ip, sp\n\t"
+        "stmfd sp!, {fp, ip, lr, pc}\n\t"
+        "sub fp, ip, #4\n\t"
+        "ldmfd sp, {fp, sp, pc}\n\t"
+    );
 
-        //KernelExit(task, &request);
-        HandleRequest(request);
-    }
+
+
+    // TaskDescriptor taskPool[MAX_NUM_TASKS];
+    // InitKernel(taskPool);
+
+    // for (;;)
+    // {
+    //     int request = 0;
+    //     TaskDescriptor *task = Scheduler();
+
+    //     if (task == NULL)
+    //     {
+    //         break;
+    //     }
+
+    //     //KernelExit(task, &request);
+    //     HandleRequest(request);
+    // }
 
     return 0;
     // bwsetfifo( COM2, OFF );
