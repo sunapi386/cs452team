1fa817122c6bc62ca0042fd5423dfb8991d2cfde
Jason Sun <jason.sun@uwaterloo.cam>
2015-05-23 18:36:46 -0400

    kernel request structure and syscall

--- kernel.c
+++ kernel.c
@@ -6,6 +6,14 @@
 #include <scheduler.h>
 #include <context_switch.h>
 
+typedef struct Request {
+    TaskDescriptor *task;
+    unsigned int call_id;
+    unsigned int arg0;
+    unsigned int arg1;
+} Request;
+
+
 void firstTask() {
     bwprintf(COM2, "First task!\n\r");
     volatile int tid = MyTid();
@@ -35,26 +43,33 @@ void InitKernel(TaskDescriptor pool[])
     InitScheduler();
 }
 
-void HandleRequest(TaskDescriptor *td, int request)
-{
-    // TODO: Enums for requests?
-    switch (request)
-    {
-    case SyscallCreate: // User task called Create()
-        // TODO: CreateTask() function
-        break;
-    case SyscallMyTid:
-        bwprintf(COM2, "tid: %d", td->id);
-        td->ret = td->id;
-        break;
-    case SyscallMyParentTid:
-        break;
-    case SyscallPass:
-        break;
-    case SyscallExit:
-        break;
-    default:
-        break;
+void HandleRequest(Request *req) {
+    switch (req->call_id) {
+        case SYS_CREATE: // User task called Create()
+                DEFAULT_STACK_SIZE, taskID(req->task)
+            unsigned int priority = req->arg0;
+            void (*code)(void) = req->arg1;
+            unsigned int stack_size = TASK_STACK_SIZE;
+            int parent_id = req->task->parent_id;
+            int ret = taskCreate(priority, code, stack_size, parent_id);
+            taskSetReturnValue(req->task, ret);
+            break;
+        case SYS_MY_TID:
+            int ret = td->id;
+            taskSetReturnValue(req->task, ret);
+            break;
+        case SYS_MY_PARENT_TID:
+            int ret = req->parent_id;
+            taskSetReturnValue(req->task, ret);
+            break;
+        case SYS_PASS:
+            break;
+        case SYS_EXIT:
+            taskExit();
+            break;
+        default:
+            bwprintf(COM2, "Invalid syscall call_id %u!", req->call_id);
+            break;
     }
 }
 
@@ -67,7 +82,7 @@ int main()
     //TaskDescriptor *first = &taskPool[0];
 
     TaskDescriptor first;
-    
+
     first.id = 5;
     first.priority = 0;
     unsigned int *ptr = first.stack;
@@ -82,17 +97,17 @@ int main()
 
     // r2 - pc (code address)
     *(first.sp - 11) = (unsigned int)(firstTask); // + 2195456
-    
+
     // r3 - user cpsr
     *(first.sp - 10) = 16;
-    
+
     // 12 registers will be poped from user stack
     first.sp -= 11;
 
     volatile int request;
     // bwputr(COM2, &request);
     KernelExit(&first, &request);
-    
+
     HandleRequest(&first, request);
 
     KernelExit(&first, &request);
@@ -110,7 +125,7 @@ int main()
          {
              break;
          }
-        
+
          KernelExit(task, &request);
          HandleRequest(request);
     }*/
