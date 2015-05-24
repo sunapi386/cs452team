cacc5ee7aabcb1707bf0905607b2ee9e594f59a5
Jason Sun <jason.sun@uwaterloo.cam>
2015-05-24 15:31:59 -0400

    syscall code spacing'

--- syscall.c
+++ syscall.c
@@ -2,42 +2,36 @@
 
 static Syscall s;
 
-// Don't modify this! I know there isn't are return statement!
+// Don't modify this! I know there isn't a return statement!
 // It's magic! (It needs the request parameter too)
-int swi(Syscall *request)
-{
+int swi(Syscall *request) {
     asm volatile("swi");
 }
 
-int Create(int priority, void (*code) ())
-{
+int create(int priority, void (*code) ()) {
     s.type = SYS_CREATE;
     s.arg1 = priority;
     s.arg2 = (unsigned int)code;
     return swi(&s);
 }
 
-int MyTid()
-{
+int myTid() {
     s.type = SYS_MY_TID;
     return swi(&s);
 }
 
-int MyParentTid()
-{
+int myParentTid() {
     s.type = SYS_MY_PARENT_TID;
     return swi(&s);
 }
 
 
-void Pass()
-{
+void pass() {
     s.type = SYS_PASS;
     swi(&s);
 }
 
-void Exit()
-{
+void exit() {
     s.type = SYS_EXIT;
     swi(&s);
 }
