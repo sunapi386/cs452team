78adeacbc3b996b66a9982254562d8dc29b2ba8f
Shuo Chang <s34chang@uwaterloo.ca>
2015-05-24 16:11:22 -0400

    Revert back syscall naming because gcc has duplicate exit function

--- syscall.c
+++ syscall.c
@@ -2,37 +2,43 @@
 
 static Syscall s;
 
-// Don't modify this! I know there isn't a return statement!
+// Don't modify this! I know there isn't are return statement!
 // It's magic! (It needs the request parameter too)
-static inline int swi(Syscall *request) {
+int swi(Syscall *request)
+{
     register unsigned int arg0_register asm("r0") = request;
     asm volatile("swi");
 }
 
-int create(int priority, void (*code) ()) {
+int Create(int priority, void (*code) ())
+{
     s.type = SYS_CREATE;
     s.arg1 = priority;
     s.arg2 = (unsigned int)code;
     return swi(&s);
 }
 
-int myTid() {
+int MyTid()
+{
     s.type = SYS_MY_TID;
     return swi(&s);
 }
 
-int myParentTid() {
+int MyParentTid()
+{
     s.type = SYS_MY_PARENT_TID;
     return swi(&s);
 }
 
 
-void pass() {
+void Pass()
+{
     s.type = SYS_PASS;
     swi(&s);
 }
 
-void exit() {
+void Exit()
+{
     s.type = SYS_EXIT;
     swi(&s);
 }
