d1f707285d43e4831ad0962379445515c5277705
Jason Sun <jason.sun@uwaterloo.cam>
2015-05-24 02:39:23 -0400

    added some more task

--- task.c
+++ task.c
@@ -5,7 +5,7 @@ static int global_next_unique_task_id;
 static TaskDescriptor *global_active_taskdescriptor;
 static TaskDescriptor global_task_table[TASK_MAX_TASKS];
 
-void initTaskSystem() {
+void initTaskSystem(void (*firstTask)(void)) {
     global_next_unique_task_id = 1;
     global_active_taskdescriptor = NULL;
     global_current_stack = (unsigned int *) TASK_STACK_HIGH;
@@ -18,12 +18,15 @@ void initTaskSystem() {
         task->spsr = 0;
         task->next = NULL;
     }
-    // setup first task
-    if( taskCreate(1, initalTask, ))
+    // setup first task, kernel_task
+    int ret = taskCreate(1, firstTask, TASK_STACK_SIZE, -1);
+    if( ret < 0 ) {
+        bwprintf( COM2, "FATAL: initTaskSystem fail creating first task %d.\n\r", ret );
+    }
 }
 
 
-int taskCreate(unsigned int priority, void (*code)(void), unsigned int stack_size, int parent_id) {
+int taskCreate(int priority, void (*code)(void), int parent_id) {
     if( priority < 0 || priority >= TASK_MAX_PRIORITY || code == NULL ) {
         return -1; // invalid params
     }
@@ -34,18 +37,21 @@ int taskCreate(unsigned int priority, void (*code)(void), unsigned int stack_siz
         return -3; // stack out of bounds
     }
 
-    // FIXME:
-    // No recycling task ids; once all the task ids been given out, it will fail to create new tasks.
-    int new_task_id = taskMakeId();
+    // FIXME: Jason: Recycle tasks.
+    // Once all the task ids been given out, it will fail to create new tasks.
+    int task_table_index = taskFindFreeTaskTableIndex();
+    int unique_id = global_next_unique_task_id++;
+    int new_task_id = taskMakeId( index, priority, unique_id );
     TaskDescriptor *new_task = &global_task_table[new_task_id];
     new_task->id = new_task_id;
     new_task->parent_id = parent_id;
-    new_task->ret = 0; // FIXME: What should this be, 0?
+    new_task->ret = 0;
     new_task->sp = taskMakeSp(new_task_id);
     new_task->spsr = UserMode | DisableIRQ | DIsableFIQ;
-    new_task->next = NULL; // FIXME: Check that scheduler takes care of this
+    new_task->next = NULL; // FIXME: Shuo: Check that scheduler takes care of this
 
     return new_task_id;
 }
 
+int taskFindFreeTaskTableIndex(); // FIXME: Jason: Implement this
 
--- task.h
+++ task.h
@@ -1,6 +1,7 @@
 #ifndef __TASK_H
 #define __TASK_H
 
+// FIXME: Shuo please verify this.
 // Trap size is # of registers pushed on stack during c-switch. Layout:
 // sp + 0 = r15 (pc)
 // sp + 1 = r0
@@ -29,20 +30,28 @@
 // each task has own trapframe and stack
 #define TASK_STACK_SIZE     1024
 #define TASK_STACK_HIGH     0x500000
-#define TASK_STACK_LOW      (TASK_STACK_HIGH - TASK_MAX_TASKS * (TASK_STACK_SIZE + TASK_TRAP_SIZE))
+#define TASK_STACK_LOW      (TASK_STACK_HIGH - \
+                            TASK_MAX_TASKS * (TASK_STACK_SIZE + TASK_TRAP_SIZE))
 
 
 /**
 Task.h
-Each task has a task descriptor (TD), which is the most important data structure in the kernel. The TDs should be local to the kernel, that is, on the kernel stack, allocated when the kernel begins executing. Each TD must either include
+Each task has a task descriptor (TD), which is the most important data structure
+in the kernel. The TDs should be local to the kernel, that is, on the kernel
+stack, allocated when the kernel begins executing. Each TD must either include
 1. a task identifier unique to this instance of the task,
 2. the task's state,
 3. the task's priority,
 4. the task identifier of the task's parent, the task that created this task,
 5. a stack pointer pointing to this task's private memory, and
 6. the task's return value, which is to be returned to the task when it
-next executes, or be able to calculate it using one or more fields of the td. The TD may include other fields. For example, a popular priority queue implementation is a doubly-linked circular list, using pointers to the TDs of tasks ahead and behind the task in the queue that are part of the TD.
-A good rule-of-thumb is that values accessed only by the context switch can be on the stack of the user task; other values should be in the task descriptor.*/
+next executes, or be able to calculate it using one or more fields of the td.
+The TD may include other fields. For example, a popular priority queue
+implementation is a doubly-linked circular list, using pointers to the TDs of
+tasks ahead and behind the task in the queue that are part of the TD.
+A good rule-of-thumb is that values accessed only by the context switch can be
+on the stack of the user task; other values should be in the task descriptor.
+*/
 
 typedef struct TaskDescriptor {
     int id;
@@ -56,8 +65,13 @@ typedef struct TaskDescriptor {
 
 void initTaskSystem();
 
-int taskCreate(unsigned int priority, void (*code)(void),
-    unsigned int stack_size, int parent_id);
+/* Returns:
+   -1: invalid priority or code pointer
+   -2: too many tasks created
+   -3: no more stack space
+   non-negative: newly created task id
+ */
+int taskCreate(unsigned int priority, void (*code)(void), int parent_id);
 
 void taskSetReturnValue(TaskDescriptor *task, int ret);
 
@@ -71,7 +85,7 @@ static inline unsigned int taskCalcStackHigh(int task_id) {
     return TASK_STACK_HIGH - (TASK_STACK_SIZE * next_task_id + TASK_TRAP_SIZE);
 }
 
-// Doesn't validate input. Make sure 0 <= {index,priority,unique} < TASK_{,PRIORITY,UNIQUE}_BITS
+// Make sure 0 <= {index,priority,unique} < TASK_{,PRIORITY,UNIQUE}_BITS
 static inline int taskMakeId(int index, int priority, int unique) {
     return
         (index << TASK_INDEX_OFFSET) |
@@ -81,7 +95,28 @@ static inline int taskMakeId(int index, int priority, int unique) {
 
 // FIXME
 static inline unsigned int *taskMakeSp(int task_id) {
+
     return
 }
+
+
+
+
+inline int taskGetMyId(TaskDescriptor *task) {
+    return task->id;
+}
+
+inline int taskGetPriority(TaskDescriptor *task) {
+    return (task->id & TASK_PRIORITY_MASK) >> TASK_PRIORITY_OFFSET;
+}
+
+inline int taskGetMyParentId(TaskDescriptor *task) {
+    return task->parent_id;
+}
+
+inline void taskSetReturnValue(TaskDescriptor *task, int ret) {
+    *(task->ret) = ret;
+}
+
 
 #endif /* __TASK_H */
