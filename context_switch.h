de837e0c0d144408e273fd9ff592c11ad6254336
Jason Sun <jason.sun@uwaterloo.cam>
2015-05-24 21:19:08 -0400

    volatile task descriptor downcast to non-volatile to get rid of gcc warning

--- context_switch.h
+++ context_switch.h
@@ -1,3 +1,3 @@
-void KernelExit(struct TaskDescriptor *task, struct Syscall **request);
+void KernelExit(volatile struct TaskDescriptor *task, struct Syscall **request);
 void KernelEnter();
 
--- kernel.c
+++ kernel.c
@@ -12,10 +12,8 @@ void initKernel() {
     // Initialize swi jump table to kernel entry point
     *(unsigned int *)(0x28) = (unsigned int)(&KernelEnter);
     initTaskSystem();
-
     initScheduler();
 
-    // setup first task, kernel_task
     TaskDescriptor *initialTask = taskCreate(1, &userModeTask, -1);
     if( ! initialTask ) {
         bwprintf( COM2, "FATAL: fail creating first task.\n\r" );
@@ -26,49 +24,48 @@ void initKernel() {
 
 void handleRequest(TaskDescriptor *td, Syscall *request) {
     switch (request->type) {
-    case SYS_CREATE: {
-        TaskDescriptor *task = taskCreate(request->arg1, (void*)request->arg2, td->parent_id);
-        if (task) {
-            queueTask(task);
-            td->ret = taskGetUnique(task);
+        case SYS_CREATE: {
+            TaskDescriptor *task = taskCreate(request->arg1, (void*)request->arg2, td->parent_id);
+            if (task) {
+                queueTask(task);
+                td->ret = taskGetUnique(task);
+            }
+            else {
+                td->ret = -1;
+            }
+            break;
         }
-        else {
-            td->ret = -1;
-        }
-        break;
-    }
-    case SYS_MY_TID:
-        td->ret = taskGetUnique(td);
-        break;
-    case SYS_MY_PARENT_TID:
-        td->ret = taskGetMyParentUnique(td);
-        break;
-    case SYS_PASS:
-        break;
-    case SYS_EXIT:
-        return;
-    default:
-        bwprintf(COM2, "Invalid syscall %u!", request->type);
-        break;
+        case SYS_MY_TID:
+            td->ret = taskGetUnique(td);
+            break;
+        case SYS_MY_PARENT_TID:
+            td->ret = taskGetMyParentUnique(td);
+            break;
+        case SYS_PASS:
+            break;
+        case SYS_EXIT:
+            return;
+        default:
+            bwprintf(COM2, "Invalid syscall %u!", request->type);
+            break;
     }
 
     // requeue the task if we haven't returned (from SYS_EXIT)
     queueTask(td);
 }
 
-int main()
-{
+int main() {
     initKernel();
-
     volatile TaskDescriptor *task = NULL;
-    for(task = schedule(); ; task = schedule()) {
-         Syscall **request = NULL;
-         if (task == NULL)  {
-             bwprintf(COM2, "No tasks scheduled; exiting...\n\r");
-             break;
-         }
-         KernelExit(task, request);
-         handleRequest(task, *request);
+
+    for(task = schedule() ; ; task = schedule()) {
+        Syscall **request = NULL;
+        if (task == NULL) {
+            bwprintf(COM2, "No tasks scheduled; exiting...\n\r");
+            break;
+        }
+        KernelExit(task, request);
+        handleRequest((TaskDescriptor *)task, *request);
     }
 
     return 0;
