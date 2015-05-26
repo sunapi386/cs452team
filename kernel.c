22ed64f5665f6c9eed5b3c745b8dd2d9df50d328
Shuo Chang <s34chang@uwaterloo.ca>
2015-05-26 17:41:17 -0400

    more work on handleSend/handleReceive

--- kernel.c
+++ kernel.c
@@ -24,6 +24,17 @@ void initKernel() {
 
 static TaskQueue sendQueues[128];
 
+// NOTE: Insecure (does not detect overlapping memory)
+void memcpy(void *dest, const void *src, unsigned int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        *(unsigned char *)dest = *(unsigned char *)src;
+        ++dest;
+        ++src;
+    }
+}
+
 void handleSend(TaskDescriptor *sending_task, Syscall *request)
 {
     int tid = (int)request->arg1;
@@ -31,14 +42,28 @@ void handleSend(TaskDescriptor *sending_task, Syscall *request)
     unsigned int msglen = request->arg3;
     void *reply = (void *)request->arg4;
     unsigned int replylen = request->arg5;
-    TaskDescriptor *receiving_task = NULL; // FIXME
 
-    //1) receiving task is rcv_blk (receive first)
+    TaskDescriptor *receiving_task = taskGetTDById(tid);
+
+    //1) receiving task is rcv_blk ( first)
     // copy msg from here to receiving task's stack
     if (receiving_task->status == receive_block)
     {
+        // memcpy(dst, src, len)
+        memcpy(receiving_task->receive,
+               msg,
+               msglen < receiving_task->receive_len ? msglen : receiving_task->receive_len);
+
+
+        // set *tid of sending task
+        *(receiving_task->sender_id) = tid;
+
         // update sending_task's status to reply_block
         sending_task->status = reply_block;
+
+        // update receiving_task's status to none and requeue it
+        receiving_task->status = none;
+        queueTask(receiving_task);
     }
     // 2) tid task is not receive_block (send first)
     // queue sending_task to send_queue of receiving_task
@@ -46,7 +71,7 @@ void handleSend(TaskDescriptor *sending_task, Syscall *request)
     {
         // get receiving_task's send queue
         TaskQueue *q = &sendQueues[tid];
-    
+
         if (q->tail == NULL)
         {
             // empty queue
@@ -60,7 +85,10 @@ void handleSend(TaskDescriptor *sending_task, Syscall *request)
             q->tail->send_next = sending_task;
             q->tail = sending_task;
         }
-        
+
+        sending_task->send = msg;
+        sending_task->send_len = msglen;
+
         // update sending_task status to send_block
         sending_task->status = send_block;
     }
@@ -77,19 +105,43 @@ void handleReceive(TaskDescriptor *receiving_task, Syscall *request)
     // if send queue empty, receive block
     if (q->head == NULL)
     {
+        receiving_task->sender_id = tid;
+        receiving_task->receive = msg;
+        receiving_task->receive_len = msglen;
         receiving_task->status = receive_block;
     }
     // if send queue not empty, dequeue first task, copy msg to msg
     else
     {
-        // dequeue first sending task
+        // dequeue the first sending task
         TaskDescriptor *sending_task = q->head;
+        q->head = sending_task->send_next;
+        sending_task->send_next = NULL;
+        if (q->tail == sending_task)
+        {
+            // that was the only task in the queue
+            q->tail = NULL;
+        }
 
-        // copy msg
-        // sending_task->msg
+        // copy data from sending_task's buffer to receiver's buffer 
+        memcpy(msg,
+               sending_task->send,
+               msglen < sending_task->send_len ? msglen : sending_task->send_len);
 
-        // ready to reply at this point
+        // set tid of sender
+        *tid = taskGetUnique(receiving_task);
+
+        // sender: reply block
+        // receiver: ready to run
+        sending_task->status = reply_block;
+        receiving_task->status = none;
+        queueTask(receiving_task);
     }
+}
+
+void handleReply()
+{
+
 }
 
 void handleRequest(TaskDescriptor *td, Syscall *request) {
@@ -128,7 +180,6 @@ void handleRequest(TaskDescriptor *td, Syscall *request) {
             bwprintf(COM2, "Invalid syscall %u!", request->type);
             break;
     }
-
     // requeue the task if we haven't returned (from SYS_EXIT)
     queueTask(td);
 }
@@ -143,6 +194,7 @@ int main() {
             bwprintf(COM2, "No tasks scheduled; exiting...\n\r");
             break;
         }
+        
         KernelExit(task, request);
         handleRequest((TaskDescriptor *)task, *request);
     }
