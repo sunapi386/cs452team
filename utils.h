11c8b96f09c78fdd2da4a34820aaa2b989a6787e
Shuo Chang <s34chang@uwaterloo.ca>
2015-05-27 13:08:17 -0400

    fix header parameter name inconsistency for strlen

--- utils.h
+++ utils.h
@@ -6,6 +6,6 @@ typedef unsigned int size_t;
 void memcpy(void *dest, const void *src, size_t n);
 int strcmp (const char * dst, const char * src);
 char * strncpy(char *dst, const char *src, size_t n);
-size_t strlen(const char *src);
+size_t strlen(const char *str);
 
 #endif
