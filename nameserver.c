569d4d6cef2640b37130554ffde45eff62c2212c
Jason Sun <jason.sun@uwaterloo.cam>
2015-05-27 01:04:44 -0400

    fixed some stuff in nameserver but cant compile switch, broken gcc?

--- nameserver.c
+++ nameserver.c
@@ -2,24 +2,22 @@
 #include <task.h>
 #include <bwio.h>
 #include <utils.h>
+#include <syscall.h>
 
-#define NS_PRIORITY 1 // nameserver should have an appropriate priority
-#define NS_TID  makeTid(1, NS_PRIORITY, 1)
+#define NS_TID  1
 
 #define NS_MAX_NAME 16
 #define NS_MAX_REGIST_SIZE 256
 
-enum {
-    SUCCESS = 0,
-    ERROR = -1,
-    FULL = -2,
-    NO_NAMESERVER = -3,
-    NO_TASK = -4,
-};
+static const int    SUCCESS = 0;
+static const int    ERROR = -1;
+static const int    FULL = -2;
+static const int    NO_NAMESERVER = -3;
+static const int    NO_TASK = -4;
 
 typedef struct {
     char name[NS_MAX_NAME];
-    enum type { REGISTER_AS, WHO_IS };
+    enum { REGISTER_AS, WHO_IS, } type;
 } NSRequest;
 
 
@@ -35,47 +33,52 @@ void nameserverTask() {
     NSRequest request;
 
     for(;;) {
-        int recv_size = Receive(&sender_tid, &request, sizeof(NSRequest));
+        int recv_size = Receive(&sender_tid, (void *)&request, sizeof(NSRequest));
         if(recv_size != sizeof(NSRequest)) {
-            Reply(sender_tid, &(ERROR), sizeof(int));
+            Reply(sender_tid, (void *)&ERROR, sizeof(int));
             continue;
         }
+
         switch (request.type) {
             case REGISTER_AS:
-                if(num_registered > NS_MAX_REGIST_SIZE) {
-                    Reply(sender_tid, &(FULL), sizeof(int));
-                    break;
-                }
-                int i;
-                for(i = 0; i < num_registered; i++) {
-                    if(strcmp(request.name, registrations[i].name) == 0) {
-                        break; // and overwrite name at i
-                    }
-                }
-                if(i == num_registered) { // create an entry
-                    num_registered++;
-                    strncpy( registrations[i].name, request.name, NS_MAX_NAME );
-                }
-                registrations[i].tid = sender_tid;
-                Reply(sender_tid, &(SUCCESS), sizeof(int));
+                // if(num_registered > NS_MAX_REGIST_SIZE) {
+                //     Reply(sender_tid, (void *)&FULL, sizeof(int));
+                //     break;
+                // }
+                // int i;
+                // for(i = 0; i < num_registered; i++) {
+                //     if( strcmp( registrations[i].name, NULL ) == 0) {
+                //         break; // and overwrite name at i
+                //     }
+                // }
+                // if(i == num_registered) { // create an entry
+                //     num_registered++;
+                //     strncpy( registrations[i].name, request.name, NS_MAX_NAME );
+                // }
+                // registrations[i].tid = sender_tid;
+                // Reply(sender_tid, (void *)&SUCCESS, sizeof(int));
 
-                break; // REGISTER_AS
+                // break; // REGISTER_AS
 
             case WHO_IS:
-                for(int i = 0; i < num_registered; i++) {
-                    if(strcmp(registrations[i].name, request.name) == 0) {
-                        Reply(sender_tid, &(registrations[i].tid), sizeof(int));
-                        break; // resolved whois name to a tid
-                    }
-                    // no registered tid for that name
-                    Reply(sender_tid, &(NO_TASK), sizeof(int));
 
-                }
+                // for(int i = 0; i < num_registered; i++) {
+                //     if( strcmp( registrations[i].name, request.name) == 0) {
+                //         Reply(sender_tid, (void *)&(registrations[i].tid), sizeof(int));
+                //         break; // resolved whois name to a tid
+                //     }
+                //     // no registered tid for that name
+                //     Reply(sender_tid, &(NO_TASK), sizeof(int));
+                // }
+                // break; // WHO_IS
 
-                break; // WHO_IS
-            default:
-                Reply( sender_tid, &(ERROR), sizeof(int) );
+
+            // default:
+                // Reply( sender_tid, (void *)&ERROR, sizeof(int) );
         } // switch
+
+
+
     } // for
 }
 
@@ -97,7 +100,7 @@ int WhoIs(char *name) {
     request.type = WHO_IS;
     strncpy(request.name, name, NS_MAX_NAME);
     int reply;
-    int bytes = Send( NS_TID, &request, sizeof(NSRequest), &reply, sizeof(int));
+    int ret = Send( NS_TID, &request, sizeof(NSRequest), &reply, sizeof(int));
     // if -2 then task id is not an existing task
     return ret == sizeof(int) ? reply : (ret == -2 ? NO_NAMESERVER : ERROR);
 }
