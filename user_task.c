49879f378d397a0f9e3530006eedfc7f99d3595e
Jason Sun <jason.sun@uwaterloo.cam>
2015-05-27 01:55:23 -0400

    message passing user_task

--- user_task.c
+++ user_task.c
@@ -6,9 +6,11 @@ static void childTask() {
     int tid = MyTid();
     int p_tid = MyParentTid();
 
-    bwprintf(COM2, "Task: %d, Parent: %d\r\n", tid, p_tid);
-    Pass();
-    bwprintf(COM2, "Task: %d, Parent: %d\r\n", tid, p_tid);
+    char *msg = "hi";
+    bwprintf(COM2, "Task: %d, Parent: %d Sending: %s.\r\n", tid, p_tid, msg);
+    char reply[3];
+    int len = Send(p_tid, msg, 3, reply, 3);
+    bwprintf(COM2, "Task: %d, Parent: %d Got reply(%d): %s.\r\n", tid, p_tid, len, reply);
     Exit();
 }
 
@@ -18,7 +20,15 @@ void userModeTask() {
         bwprintf(COM2, "Created: %d\r\n", Create( (i < 2) ? 2 : 0, childTask ));
     }
 
+    for(int i = 0; i < 4; i++) {
+        char msg[3];
+        int tid = -1;
+        int len = Receive(&tid, msg, 3);
+        bwprintf(COM2, "1st task received from %d message(%d): %s. ", tid, len, msg);
+        char *reply = "IH";
+        Reply(tid, reply, 3);
+    }
+
     bwputstr(COM2, "First: exiting\r\n");
     Exit();
-    bwputstr(COM2, "First: failed to exit!\r\n");
 }
