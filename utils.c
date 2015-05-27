660dfcda8ae13271a286660cf1a3884afebc0f2b
Jason Sun <jason.sun@uwaterloo.cam>
2015-05-27 00:24:11 -0400

    typedef size_t

--- utils.c
+++ utils.c
@@ -1,7 +1,7 @@
 #include <utils.h>
 
 // NOTE: Insecure (does not detect overlapping memory)
-void memcpy(void *dest, const void *src, unsigned int n) {
+void memcpy(void *dest, const void *src, size_t n) {
     for (int i = 0; i < n; i++) {
         *(unsigned char *)dest = *(unsigned char *)src;
         ++dest;
--- utils.h
+++ utils.h
@@ -1,7 +1,9 @@
 #ifndef __UTILS_H
 #define __UTILS_H
 
-void memcpy(void *dest, const void *src, unsigned int n);
+typedef unsigned int size_t;
+
+void memcpy(void *dest, const void *src, size_t n);
 int strcmp(char *s1, char *s2);
 char * strncpy(char *dst, const char *src, size_t n);
 
