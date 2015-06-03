69782121b68fa1875b93bcdc20a5b534a4fcf750
Jason Sun <jason.sun@uwaterloo.cam>
2015-06-03 17:22:58 -0400

    debug assert

--- include/debug.h
+++ include/debug.h
@@ -5,16 +5,23 @@
 
 #ifdef PRODUCTION
 
-#define debug(fmt, ...)     (void)(0)
+#define debug(fmt, ...)     ((void)0)
+#define assert(ignore)      ((void)0)
 
 #else // ! PRODUCTION
 
 #define debug(fmt, ...)     bwprintf(COM2,  \
-    "%s @ %s - %d: " fmt "\r\n",            \
-    __FUNCTION__,                           \
+    "[%s:%d] %s: " fmt "\r\n",              \
     __FILE__,                               \
     __LINE__,                               \
+    __FUNCTION__,                           \
     ## __VA_ARGS__                          \
+)
+
+#define assert(expr) (                          \
+    (expr) ?                                    \
+    (void)(0) :                                 \
+    debug("\tASSERTION \"%s\" FAILED!", #expr)  \
 )
 
 #endif // PRODUCTION
