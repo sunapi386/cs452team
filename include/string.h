5ee6cfb54f9927ca230779a77855e041d1873010
Jason Sun <jason.sun@uwaterloo.cam>
2015-06-16 17:23:24 -0400

    string ported in printf

--- include/string.h
+++ include/string.h
@@ -72,10 +72,138 @@ static inline char *sbuf(String *s) {
     return s->buf;
 }
 
-static inline void sformat(String *s ) {
-// TODO: if needed, make sformat (similar to printf)
-// for now, just use sputc to manually build a string
-    (void)s;
+
+
+#define __va_argsiz(t)  \
+        (((sizeof(t) + sizeof(int) - 1) / sizeof(int)) * sizeof(int))
+
+#define va_start(ap, pN) ((ap) = ((va_list) __builtin_next_arg(pN)))
+
+#define va_end(ap)  ((void)0)
+
+#define va_arg(ap, t)   \
+         (((ap) = (ap) + __va_argsiz(t)), *((t*) (void*) ((ap) - __va_argsiz(t))))
+
+
+static inline int _sa2d( char ch ) {
+    if( ch >= '0' && ch <= '9' ) return ch - '0';
+    if( ch >= 'a' && ch <= 'f' ) return ch - 'a' + 10;
+    if( ch >= 'A' && ch <= 'F' ) return ch - 'A' + 10;
+    return -1;
+}
+
+static inline char _sa2i( char ch, char **src, int base, int *nump ) {
+    int num, digit;
+    char *p;
+
+    p = *src; num = 0;
+    while( ( digit = _sa2d( ch ) ) >= 0 ) {
+        if ( digit > base ) break;
+        num = num*base + digit;
+        ch = *p++;
+    }
+    *src = p; *nump = num;
+    return ch;
+}
+
+static inline void _sputw(String *s, int n, char fc, char *bf) {
+    char ch;
+    char *p = bf;
+
+    while( *p++ && n > 0 ) n--;
+    while( n-- > 0 ) sputc(s, fc);
+    while( ( ch = *bf++ ) ) sputc(s, ch);
+}
+
+static inline void _sui2a(unsigned int num, unsigned int base, char *bf) {
+    int n = 0;
+    int dgt;
+    unsigned int d = 1;
+
+    while( (num / d) >= base ) d *= base;
+    while( d != 0 ) {
+        dgt = num / d;
+        num %= d;
+        d /= base;
+        if( n || dgt > 0 || d == 0 ) {
+            *bf++ = dgt + ( dgt < 10 ? '0' : 'a' - 10 );
+            ++n;
+        }
+    }
+    *bf = 0;
+}
+
+
+static inline void _si2a( int num, char *bf ) {
+    if( num < 0 ) {
+        num = -num;
+        *bf++ = '-';
+    }
+    _sui2a( num, 10, bf );
+}
+
+
+static inline void sformat(String *s, char *fmt, va_list va) {
+    char bf[12];
+    char ch, lz;
+    int w;
+
+    while ( ( ch = *(fmt++) ) ) {
+        if ( ch != '%' )
+            sputc(s, ch );
+        else {
+            lz = 0; w = 0;
+            ch = *(fmt++);
+            switch ( ch ) {
+            case '0':
+                lz = 1; ch = *(fmt++);
+                break;
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                ch = _sa2i( ch, &fmt, 10, &w );
+                break;
+            }
+            switch( ch ) {
+            case 0: return;
+            case 'c':
+                sputc(s, va_arg( va, char ) );
+                break;
+            case 's':
+                _sputw(s, w, 0, va_arg( va, char* ));
+                break;
+            case 'u':
+                _sui2a(va_arg( va, unsigned int ), 10, bf );
+                _sputw(s, w, lz, bf );
+                break;
+            case 'd':
+                _si2a(va_arg( va, int ), bf);
+                _sputw(s, w, lz, bf);
+                break;
+            case 'x':
+                _sui2a(va_arg( va, unsigned int ), 16, bf);
+                _sputw(s, w, lz, bf);
+                break;
+            case '%':
+                sputc(s, ch);
+                break;
+            }
+        }
+    }
+}
+
+
+static inline void sprintf(String *s, char *fmt, ... ) {
+    va_list va;
+    va_start(va, fmt);
+    sformat(s, fmt, va );
+    va_end(va);
 }
 
 
