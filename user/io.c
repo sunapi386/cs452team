f907d1d561833dd7aaa3b758c81fe4939b9457db
Shuo Chang <s34chang@uwaterloo.ca>
2015-06-16 15:45:43 -0400

    train inbound server & notifier

--- user/io.c
+++ user/io.c
@@ -115,7 +115,6 @@ static void echoCourier() {
     }
 }
 
-// server that waits for receiveNotifier to deliver a character
 void monitorInServer() {
     char c = 0;
     int tid = 0, courierTid = 0;
@@ -208,7 +207,6 @@ static void monitorOutNotifier() {
     }
 }
 
-// sendServer handles outbound traffic on COM ports
 void monitorOutServer() {
     int tid = 0;
     char * sendAddr = 0;
@@ -302,6 +300,82 @@ void monitorOutServer() {
                 // reset 'seen transmit int' state
                 sendAddr = 0;
             }
+        default:
+            break;
+        }
+    }
+}
+
+static void trainInNotifier() {
+    int pid = MyParentTid();
+    IOReq req = {
+        .type = NOTIFICATION
+    };
+
+    for (;;) {
+        req.data = (unsigned int)AwaitEvent(UART1_RECV_EVENT);
+        Send(pid, &req, sizeof(req), 0, 0);
+    }
+}
+
+void trainInServer() {
+    char c = 0;
+    int tid = 0;
+    char taskb[128];
+    char charb[1024];
+    CBuffer taskBuffer;
+    CBuffer charBuffer;
+    CBufferInit(&taskBuffer, taskb, 128);
+    CBufferInit(&charBuffer, charb, 1024);
+
+    IOReq req = {
+        .type = -1,
+        .data = '\0'
+    };
+
+    // TODO: nameserver
+    com1RecvSrvTid = MyTid();
+
+    RegisterAs("trainInServer");
+
+    // Spawn notifier
+    Create(1, &trainInNotifier);
+
+    for (;;) {
+        Receive(&tid, &req, sizeof(req));
+
+        switch (req.type) {
+        // the notifier sent us a character
+        case NOTIFICATION:
+            // unblock notifier
+            Reply(tid, 0, 0);
+
+            c = (char)req.data;
+            if(CBufferIsEmpty(&taskBuffer)) {
+                // no tasks waiting, put char in a buffer
+                CBufferPush(&charBuffer, c);
+            }
+            else {
+                // there are tasks waiting for a char
+                // unblock a task and hand it the char
+                // that we'd just acquired
+                int popped_tid = (int)CBufferPop(&taskBuffer);
+
+                Reply(popped_tid, &c, sizeof(c));
+            }
+            break;
+        // some user task called getchar
+        case GETCHAR:
+            if(CBufferIsEmpty(&charBuffer)) {
+                // no char to give back to caller; block it
+                CBufferPush(&taskBuffer, (char)tid);
+            }
+            else {
+                // characters in the buffer, return it
+                c = CBufferPop(&charBuffer);
+                Reply(tid, &c, sizeof(c));
+            }
+            break;
         default:
             break;
         }
