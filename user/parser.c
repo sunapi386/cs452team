9f549bf465dbfc30462316e40ba71193bf2f084e
Jason Sun <jason.sun@uwaterloo.cam>
2015-06-16 14:46:48 -0400

    parse carriage return

--- user/parser.c
+++ user/parser.c
@@ -58,7 +58,10 @@ typedef struct Parser {
 p->state = (c==ch) ? succ : Error;
 
 bool append_number(char c, int *num) {
-    // TODO
+    if('0' <= c && c <= '9') {
+        *num = 10 * (*num) + (int)(c - '0');
+        return true;
+    }
     return false;
 }
 
@@ -75,134 +78,134 @@ static bool parse(Parser *p, char c) {
 
 
     // only accept ascii characters and newline
-    if(0x20 <= c && c <= 0x7e) { // if printable
-        if(0x41 <= c && c <= 0x5a) {
-            c |= 0x20;
+    if(' ' <= c && c <= '~') { // if printable
+        if('A' <= c && c <= 'Z') {
+            c += ('a' - 'A');
         } // lower case any capital character
 
         switch(p->state) {
-        case Empty: {
-            switch(c) {
-                case 't': p->state = TR_T;
-                case 'r': p->state = RV_R;
-                case 's': p->state = SW_S;
-                case 'q': p->state = Q_Q;
-                default:  p->state = Error; break;
-            }
-            break;
-        }
-        case Error: {
-            break;
-        }
-
-        // ----------- tr train_number train_speed ----------- //
-        case TR_T: {
-            REQUIRE('t', TR_R);
-            break;
-        }
-        case TR_R: {
-            REQUIRE('r', TR_space_1);
-            break;
-        }
-        case TR_space_1: { // parse a train_number
-            p->data.speed.train_number = 0;
-            p->state =  append_number(c, &(p->data.speed.train_number)) ?
-                        TR_train_number :
-                        Error;
-            break;
-        }
-        case TR_train_number: {
-            if(! append_number(c, &(p->data.speed.train_number))) {
-                REQUIRE(' ', TR_space_2);
-            }
-            break;
-        }
-        case TR_space_2: { // parse a train_speed
-            p->data.speed.train_speed = 0;
-            p->state =  append_number(c, &(p->data.speed.train_speed)) ?
-                        TR_speed :
-                        Error;
-            break;
-        }
-        case TR_speed: {
-            if(! append_number(c, &(p->data.speed.train_speed))) {
-                p->state = Error;
-            }
-            break;
-        } // TR_speed
-
-        // rv train_number ----------- //
-        case RV_R: {
-            REQUIRE('v', RV_V);
-            break;
-        }
-        case RV_V: {
-            REQUIRE(' ', RV_V);
-            break;
-        }
-        case RV_space: { //
-            p->data.reverse.train_number = 0;
-            p->state =  append_number(c, &(p->data.reverse.train_number)) ?
-                        RV_train_number :
-                        Error;
-            break;
-        }
-        case RV_train_number: {
-            if(! append_number(c, &(p->data.reverse.train_number))) {
-                p->state = Error;
-            }
-            break;
-        } // RV_train_number
-
-        // sw switch_number switch_direction ----------- //
-        case SW_S: {
-            REQUIRE('w', SW_W);
-            break;
-        }
-        case SW_W: {
-            REQUIRE(' ', SW_space_1);
-            break;
-        }
-        case SW_space_1: {
-            p->data.junction.switch_number = 0;
-            p->state =  append_number(c, &(p->data.junction.switch_number)) ?
-                        SW_switch_number :
-                        Error;
-            break;
-        }
-        case SW_switch_number: {
-            if(! append_number(c, &(p->data.junction.switch_number))) {
-                REQUIRE(' ', SW_space_2);
-            }
-            break;
-        }
-        case SW_space_2: {
-            switch(c) { // either 'c' or 's'
-            case 'c':
-                p->data.junction.curved = true;
-                p->state = SW_switch_dir;
+            case Empty: {
+                switch(c) {
+                    case 't': p->state = TR_T;
+                    case 'r': p->state = RV_R;
+                    case 's': p->state = SW_S;
+                    case 'q': p->state = Q_Q;
+                    default:  p->state = Error; break;
+                }
                 break;
-            case 's':
-                p->data.junction.curved = true;
-                p->state = SW_switch_dir;
-            default:
-                p->state = Error;
             }
-            break;
-        }
-        case SW_switch_dir: {
-            p->state = Error;
-            break;
-        }
+            case Error: {
+                break;
+            }
 
-        // ----------- q ----------- //
-        case Q_Q: {
-            p->state = Error;
-            break;
-        }
+            // ----------- tr train_number train_speed ----------- //
+            case TR_T: {
+                REQUIRE('t', TR_R);
+                break;
+            }
+            case TR_R: {
+                REQUIRE('r', TR_space_1);
+                break;
+            }
+            case TR_space_1: { // parse a train_number
+                p->data.speed.train_number = 0;
+                p->state =  append_number(c, &(p->data.speed.train_number)) ?
+                            TR_train_number :
+                            Error;
+                break;
+            }
+            case TR_train_number: {
+                if(! append_number(c, &(p->data.speed.train_number))) {
+                    REQUIRE(' ', TR_space_2);
+                }
+                break;
+            }
+            case TR_space_2: { // parse a train_speed
+                p->data.speed.train_speed = 0;
+                p->state =  append_number(c, &(p->data.speed.train_speed)) ?
+                            TR_speed :
+                            Error;
+                break;
+            }
+            case TR_speed: {
+                if(! append_number(c, &(p->data.speed.train_speed))) {
+                    p->state = Error;
+                }
+                break;
+            } // TR_speed
 
-        default:
-            break;
+            // rv train_number ----------- //
+            case RV_R: {
+                REQUIRE('v', RV_V);
+                break;
+            }
+            case RV_V: {
+                REQUIRE(' ', RV_V);
+                break;
+            }
+            case RV_space: { //
+                p->data.reverse.train_number = 0;
+                p->state =  append_number(c, &(p->data.reverse.train_number)) ?
+                            RV_train_number :
+                            Error;
+                break;
+            }
+            case RV_train_number: {
+                if(! append_number(c, &(p->data.reverse.train_number))) {
+                    p->state = Error;
+                }
+                break;
+            } // RV_train_number
+
+            // sw switch_number switch_direction ----------- //
+            case SW_S: {
+                REQUIRE('w', SW_W);
+                break;
+            }
+            case SW_W: {
+                REQUIRE(' ', SW_space_1);
+                break;
+            }
+            case SW_space_1: {
+                p->data.junction.switch_number = 0;
+                p->state =  append_number(c, &(p->data.junction.switch_number)) ?
+                            SW_switch_number :
+                            Error;
+                break;
+            }
+            case SW_switch_number: {
+                if(! append_number(c, &(p->data.junction.switch_number))) {
+                    REQUIRE(' ', SW_space_2);
+                }
+                break;
+            }
+            case SW_space_2: {
+                switch(c) { // either 'c' or 's'
+                case 'c':
+                    p->data.junction.curved = true;
+                    p->state = SW_switch_dir;
+                    break;
+                case 's':
+                    p->data.junction.curved = true;
+                    p->state = SW_switch_dir;
+                default:
+                    p->state = Error;
+                }
+                break;
+            }
+            case SW_switch_dir: {
+                p->state = Error;
+                break;
+            }
+
+            // ----------- q ----------- //
+            case Q_Q: {
+                p->state = Error;
+                break;
+            }
+
+            default:
+                break;
         } // switch
 
         if(p->state == Error) {
@@ -215,7 +218,63 @@ static bool parse(Parser *p, char c) {
 
     else if(c == VT_CARRIAGE_RETURN) { // user pressed return (enter)
 
+        // should be on an end state
+        switch(p->state) {
+            case TR_speed: {
+                // check train_number and train_speed
+                int train_number = p->data.speed.train_number;
+                int train_speed = p->data.speed.train_speed;
+                if((0 <= train_number && train_number <= 80) &&
+                   (0 <= train_speed  && train_speed  <= 14)) {
+
+                }
+                else {
+                    sputstr(&disp_msg,
+                        "TR: bad train_number or train_speed\r\n");
+                }
+
+                break;
+            }
+            case RV_train_number: {
+                // check train_number
+                int train_number = p->data.reverse.train_number;
+                if(0 <= train_number && train_number <= 80) {
+
+                }
+                else {
+                    sputstr(&disp_msg,
+                        "RV: bad train_number\r\n");
+                }
+
+                break;
+            }
+            case SW_switch_dir: {
+                // check switch_number
+                int switch_number = p->data.junction.switch_number;
+                if(   1 <= switch_number && switch_number <= 18 ||
+                    153 <= switch_number && switch_number <= 156) {
+                    bool curved = p->data.junction.curved;
+
+                }
+                else { //
+                    sputstr(&disp_msg,
+                        "SW: bad switch_number\r\n");
+                }
+
+                break;
+            }
+            case Q_Q: {
+                sputstr(&disp_msg, "Quit\r\n");
+
+                run = false;
+                break;
+            }
+            default: {
+                sputstr(&disp_msg, "Command not recognized!\r\n");
+            }
+        }
     } // else if carriage return
+
     return run;
 }
 
