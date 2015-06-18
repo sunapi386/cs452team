844740f9ba72b5ab6c2a0a9c361acd3724978afe
Jason Sun <jason.sun@uwaterloo.cam>
2015-06-17 23:23:12 -0400

    set scrolling region, kind of broken

--- user/vt100.c
+++ user/vt100.c
@@ -2,25 +2,17 @@
 #include <user/vt100.h>
 #include <user/syscall.h>
 
-
 void vt_init() {
     String s;
     sinit(&s);
-    sprintf(&s, "%s", VT_CLEAR_SCREEN);
-    sputstr(&s, VT_RESET);
+    sputstr(&s, VT_CLEAR_SCREEN);
     sputstr(&s, VT_CURSOR_HIDE);
-
+    sputstr(&s, VT_RESET);
 
     // set scrolling region
-    sprintf(&s, "%c[%u;%ur", ESC, VT_PARSER_TOP, VT_PARSER_BOT);
-    // ESC  [  Pt   ;  Pb   r
-    // Selects top and bottom margins, defining the scrolling region.
-    // Pt is line number of first line in the scrolling region.
-    // Pb is line number of bottom line.
-    // If Pt and Pb are not selected, the complete screen is used (no margins).
-    sputstr(&s, "Loading...");
-    vt_pos(&s, VT_PARSER_ROW, VT_PARSER_COL);
+    sprintf(&s, "%s%d;%dr", VT_CSI, 18, 20);
     sputstr(&s, VT_CURSOR_SHOW);
+    sputstr(&s, "Loading...");
     PutString(&s);
 }
 
