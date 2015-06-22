8719a6d36d9d34e3cd2bfc11c85c41bb5018ba1b
Shuo Chang <s34chang@uwaterloo.ca>
2015-06-22 18:37:18 -0400

    removed INT_IRQ

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -25,7 +25,7 @@ void enableCache()
 {
     asm volatile(
         "mrc p15, 0, r0, c1, c0, 0\n\t"
-        "ldr r1, =0xc0001004\n\t"
+        "ldr r1, =0x00001004\n\t"
         "orr r0, r0, r1\n\t"
         "mcr p15, 0, r0, c1, c0, 0\n\t"
     );
@@ -35,7 +35,7 @@ void disableCache()
 {
     asm volatile(
         "mrc p15, 0, r0, c1, c0, 0\n\t"
-        "ldr r1, =0x1fffeffb\n\t"
+        "ldr r1, =0xffffeffb\n\t"
         "and r0, r0, r1\n\t"
         "mcr p15, 0, r0, c1, c0, 0\n\t"
     );
@@ -45,26 +45,47 @@ void idleProfiler() {
     for (;;) {
         //drawIdle(getIdlingRatio());
         //bwprintf(COM2, "_");
+        //Putc(COM2, 'i');
         Pass();
     }
 }
+
 #include <ts7200.h>
-#include <utils.h>
+
+static int clientTid = -1;
+
 void client()
 {
+    for (;;)
+    {
+        char *dataAddr = (char *)AwaitEvent(UART2_XMIT_EVENT);
+        if (dataAddr != (char *)(UART2_BASE + UART_DATA_OFFSET))
+        {
+            bwprintf(COM2, "gg %x", dataAddr);
+            bwprintf(COM2, "my tid: %d, original tid: %d", MyTid(), clientTid);
+            break;
+        }
+        *dataAddr = ' ';
+        //Putc(COM2, '*');
+    }
+    Exit();
+}
+
+void clientNotifier()
+{
+    //char *dataAddr = (char *)(UART2_BASE + UART_DATA_OFFSET);
     /*
-    char *dataAddr = (char *)(UART2_BASE + UART_DATA_OFFSET);
-    char buf[2048];
-    CBuffer rb;
-    CBufferInit(&rb, buf, 2048);
+        char buf[2048];
+        CBuffer rb;
+        CBufferInit(&rb, buf, 2048);
     */
     // unsigned int i;
-
+    //clientTid = clientTid;
     for (;;)
     {
         //AwaitEvent(UART2_XMIT_EVENT);
         //*dataAddr = '*';
-        Putc(COM2, '*');
+        Pass();
     }
     Exit();
 }
@@ -72,23 +93,25 @@ void client()
 void bootstrap()
 {
     // Create name server
-    Create (PRIORITY_NAMESERVER, nameserverTask);
+    //Create (PRIORITY_NAMESERVER, nameserverTask);
 
     // Create clock server
     Create(PRIORITY_CLOCK_SERVER, clockServerTask);
 
     // Create IO Servers
-    Create(PRIORITY_TRAIN_OUT_SERVER, trainOutServer);
-    Create(PRIORITY_TRAIN_IN_SERVER, trainInServer);
-    Create(PRIORITY_MONITOR_OUT_SERVER, monitorOutServer);
-    Create(PRIORITY_MONITOR_IN_SERVER, monitorInServer);
+    //Create(PRIORITY_TRAIN_OUT_SERVER, trainOutServer);
+    //Create(PRIORITY_TRAIN_IN_SERVER, trainInServer);
+    //Create(PRIORITY_MONITOR_OUT_SERVER, monitorOutServer);
+    //Create(PRIORITY_MONITOR_IN_SERVER, monitorInServer);
 
     // Create user task
-    Create(PRIORITY_CLOCK_DRAWER, clockDrawer);
-    Create(PRIORITY_PARSER, parserTask);
+    //Create(PRIORITY_CLOCK_DRAWER, clockDrawer);
+    //Create(PRIORITY_PARSER, parserTask);
     // Create(PRIORITY_SENSOR_TASK, sensorTask);
 
-    // Create(PRIORITY_USERTASK, client);
+    Create(PRIORITY_USERTASK, client);
+    Create(PRIORITY_USERTASK, clientNotifier);
+    //Create(PRIORITY_USERTASK, client2);
 
     // Create idle task
     Create(PRIORITY_IDLE, idleProfiler);
@@ -98,7 +121,7 @@ void bootstrap()
 }
 
 static void initKernel() {
-    //enableCache();
+    enableCache();
     initTaskSystem();
     initScheduler();
     initMessagePassing();
@@ -113,7 +136,7 @@ static void initKernel() {
     // int create_ret = taskCreate(1, interruptRaiser, 0);
     // int create_ret = taskCreate(1, userTaskK3, 0);
     // int create_ret = taskCreate(1, userTaskIdle, 31);
-    int create_ret = taskCreate(1, bootstrap, 0);
+    int create_ret = taskCreate(PRIORITY_INIT, bootstrap, 0);
 
     assert(create_ret >= 0);
     queueTask(taskGetTDById(create_ret));
@@ -123,18 +146,27 @@ static void resetKernel() {
     resetTimer();
     // resetUART();
     resetInterrupts();
-    // /disableCache();
+    disableCache();
 }
 
 static inline int handleRequest(TaskDescriptor *td) {
+    if (isHwi())
+    {
+        handleInterrupt();
+        clearHwi();
+        goto done;
+    }
+
     switch (request->type) {
-        case INT_IRQ:
-            handleInterrupt();
-            break;
         case SYS_AWAIT_EVENT:
-            td->ret = awaitInterrupt(td, request->arg1);
-            // we don't want to reschedule if the task is event blocked
-            if (td->ret == 0) return 0;
+            if (awaitInterrupt(td, request->arg1) == -1)
+            {
+                // we want rescheduling
+                td->ret = -1;
+                break;
+            }
+            // we don't want rescheduling now that it's event blocked
+            return 0;
         case SYS_SEND:
             handleSend(td, request);
             return 0;
@@ -172,6 +204,7 @@ static inline int handleRequest(TaskDescriptor *td) {
             debug("Invalid syscall %u!", request->type);
             break;
     }
+done:
     // requeue the task if we haven't returned (from SYS_EXIT)
     queueTask(td);
     return 0;
@@ -190,7 +223,6 @@ int main() {
             bwprintf(COM2, "Halt\n\r");
             break;
         }
-        request->type = INT_IRQ;
     }
     resetKernel();
     debug("No tasks scheduled; exiting...");
