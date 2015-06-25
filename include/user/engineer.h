b15bcadc1008442d9eb6acb3ca11447f6c0a1943
Jason Sun <jason.sun@uwaterloo.cam>
2015-06-25 19:25:03 -0400

    skeleton planner and engineer

---
+++ include/user/engineer.h
@@ -0,0 +1,10 @@
+#ifndef __ENGINEER_H
+#define __ENGINEER_H
+/**
+An engineer is a program that operates a train, responsible for
+driving the train, as well as the mechanical operation of the train,
+train speed.
+*/
+
+
+#endif
---
+++ include/user/planner.h
@@ -0,0 +1,12 @@
+#ifndef __PLANNER_H
+#define __PLANNER_H
+/**
+A central planner coordinates train movements and keeps track of them.
+This is done by keeping track of triggered sensors, changing turnouts,
+sending commands to trains, and keeping track of train positions.
+Finally, when pathfinding is implemented, it automatically plans a path
+for a train to reach from targets A to B.
+*/
+
+
+#endif
