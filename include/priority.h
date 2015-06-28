935333ed945d6e466826a9d47861dd9c53c8f92c
Jason Sun <jason.sun@uwaterloo.cam>
2015-06-28 02:11:25 -0400

    add ben's note about priorities

--- include/priority.h
+++ include/priority.h
@@ -1,6 +1,23 @@
 #ifndef PRIORITY_H
 #define PRIORITY_H
 
+/**
+Just a gentle reminder that task priorities moving forwards will be --very--
+important for your kernels in terms of correct behaviour.
+
+4 through 12 priority levels are likely not enough for dealing with all of
+the OS features in addition to the trains. It should be possible to get 32
+priorities working with close to (if not the same) efficiency, especially if
+you are using a naive strategy to deal with fewer priorities.
+
+In particular, it is important to use priorities in such a way that you
+guarantee the most important interrupts / handling take precedence over
+others. For instance, it should not be possible for I/O (in either
+direction) to cause clock ticks to be lost.
+
+Ben
+*/
+
 // Common system level priorities
 #define PRIORITY_INIT               0
 #define PRIORITY_NOTIFIER           1
